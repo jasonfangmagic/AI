897138aa54dac8d9324dda919bfac814ea5afa94
jasonfangmagic <jasonfangmagic@gmail.com>
2021-11-14 10:01:10 -0500

    /

---
+++ SD/Deep_Learning/CNN.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import seaborn as sns
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Dropout
