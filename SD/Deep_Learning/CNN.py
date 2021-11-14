ff710dfefe89a564125ab4e1e998170c0d29ba1c
jasonfangmagic <jasonfangmagic@gmail.com>
2021-11-14 12:11:11 -0500

    /

--- SD/Deep_Learning/CNN.py
+++ SD/Deep_Learning/CNN.py
@@ -65,3 +65,16 @@ with open("cat_dog.pickle", "wb") as f:
 #
 pickle_in = open("cat_dog.pickle", "rb")
 cnn_model = pickle.load(pickle_in)
+
+#predict model
+from tensorflow.keras.preprocessing import image
+test_image = image.load_img('Deep_Learning/Part 2 - Convolutional Neural Networks/dataset/single_prediction/cat_or_dog_1.jpg', target_size = (64, 64))
+test_image = image.img_to_array(test_image)
+test_image = np.expand_dims(test_image, axis = 0)
+result = cnn.predict(test_image)
+training_set.class_indices
+if result[0][0] == 1:
+    prediction = 'dog'
+else:
+    prediction = 'cat'
+print(prediction)
